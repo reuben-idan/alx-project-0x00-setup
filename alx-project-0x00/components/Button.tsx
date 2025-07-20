4cf829a915de86524f6eb1ef2175d1f0fab1e3bc
reuben-idan <nobeadonis2@gmail.com>
2025-07-20 19:15:04 +0000

    Refactor ButtonProps to use 'styles' prop, update Button component and landing page accordingly

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, className = "" }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles = "" }) => {
   return (
     <button
-      className={`px-4 py-2 font-semibold bg-blue-500 text-white ${className}`}
+      className={`px-4 py-2 font-semibold bg-blue-500 text-white ${styles}`}
     >
       {title}
     </button>
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,9 +6,9 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <div className="flex gap-4 mb-6">
-        <Button title="Small Rounded-sm" className="text-xs rounded-sm" />
-        <Button title="Medium Rounded-md" className="text-base rounded-md" />
-        <Button title="Large Rounded-full" className="text-lg rounded-full" />
+        <Button title="Small Rounded-sm" styles="text-xs rounded-sm" />
+        <Button title="Medium Rounded-md" styles="text-base rounded-md" />
+        <Button title="Large Rounded-full" styles="text-lg rounded-full" />
       </div>
       <Card />
       <Card />
