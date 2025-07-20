4dbc993ea7eb0caadea683cfb25870fe7797838c
reuben-idan <nobeadonis2@gmail.com>
2025-07-20 19:08:39 +0000

    Add Button component with dynamic props and demonstrate usage on landing page

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, className = "" }) => {
+  return (
+    <button
+      className={`px-4 py-2 font-semibold bg-blue-500 text-white ${className}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,15 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <div className="flex gap-4 mb-6">
+        <Button title="Small Rounded-sm" className="text-xs rounded-sm" />
+        <Button title="Medium Rounded-md" className="text-base rounded-md" />
+        <Button title="Large Rounded-full" className="text-lg rounded-full" />
+      </div>
       <Card />
       <Card />
       <Card />
